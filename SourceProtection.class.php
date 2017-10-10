c0031287d115033c2f72487f7e6990f280b4592c
Charlot <charlot@wikibase>
2017-10-10 11:21:36 +0200

    Added more security actions for users with no edit rights.

--- SourceProtection.class.php
+++ SourceProtection.class.php
@@ -12,35 +12,74 @@
 class SourceProtection {
 
 
-public static function hideSource ( SkinTemplate &$sktemplate, array &$links ) {
+	// Dive into the skin. Check is a user may edit. If not, remove tabs.
+	public static function hideSource ( SkinTemplate &$sktemplate, array &$links ) {
 		// always remove viewsource tab
-	$removeUs = array ('viewsource');
-    foreach ( $removeUs as $view ) {
-			if ( isset( $links['views'][$view] )) {
-        unset( $links['views'][$view] );
+		$removeUs = array ('viewsource');
+	    foreach ( $removeUs as $view ) {
+				if ( isset( $links['views'][$view] )) {
+	        unset( $links['views'][$view] );
+				}
+	    }
+		// grab user permissions
+	    if ( method_exists ( $sktemplate, 'getTitle' ) ) {
+	        $title = $sktemplate->getTitle();
+	    } else {
+	        $title = $sktemplate->mTitle;
+	    }
+
+	    $user_can_edit = $title->userCan( 'edit' );
+
+        //remove form_edit and history when edit is disabled
+	    if ( $user_can_edit === false) {
+		    $rem = array ('form_edit','history');
+	        foreach ( $rem as $v ) {
+	            if ( isset( $links['views'][$v] )) {
+	                unset( $links['views'][$v] );
+	            }
+	        }
+	    }
+
+		 return true;
+
+	}
+
+	// If a user has no edit rights, then make sure it is hard for him to view
+	// the source of a document
+	public static function disableActions( &$title, &$wgUser, $action, &$result ) {
+        if ( in_array( 'edit', $wgUser->getRights(), true) ) {
+            return true;
+        } else {
+			// define the actions to be blocked
+            $actionNotAllowed = array (
+                'edit',
+                'move',
+                'history',
+                'info',
+                'raw',
+                'delete',
+                'revert',
+                'revisiondelete',
+                'rollback',
+                'markpatrolled'
+            );
+			// Also disable the version difference options
+			if(isset($_GET['diff'])) {
+				return false;
 			}
-    }
-		// grab user persmissions
-    if ( method_exists ( $sktemplate, 'getTitle' ) ) {
-        $title = $sktemplate->getTitle();
-    } else {
-        $title = $sktemplate->mTitle;
-    }
-    $user_can_edit = $title->userCan( 'edit' );
-		//remove form_edit and history when edit is disabled
-    if ( $user_can_edit === false) {
-        $rem = array ('form_edit','history');
-        foreach ( $rem as $v ) {
-            if ( isset( $links['views'][$v] )) {
-                unset( $links['views'][$v] );
+            if (isset($_GET['action'])) {
+                $actie = $_GET['action'];
+                if ( in_array ($actie,$actionNotAllowed) ) {
+                    return false;
+                }
             }
+			// Any other action is fine
+            return true;
         }
-    }
+	}
 
-	 return true;
-
-}
- // prevent ShowReadOnly form to be shown
+    // prevent ShowReadOnly form to be shown
+    // We should never get here anymore, but just in case.
 	public static function doNotShowReadOnlyForm (EditPage $editPage, OutputPage $output) {
 
         if ( method_exists ( $editPage, 'getTitle' ) ) {
--- SourceProtection.php
+++ SourceProtection.php
@@ -3,7 +3,7 @@
  * The sourceProtection extension to MediaWiki allows to restrict the edit
  * access to user pages.
  *
- * @version 1.1.0 2017-08-08
+ * @version 1.1.1 2017-10-09
  *
  * @author Sen-Sai
  *
@@ -18,7 +18,7 @@
  	// Keep i18n globals so mergeMessageFileList.php doesn't break
  	$wgMessagesDirs['SourceProtection'] = __DIR__ . '/i18n';
  	wfWarn(
- 		'Deprecated PHP entry point used for WSssd extension. Please use wfLoadExtension ' .
+ 		'Deprecated PHP entry point used for SourceProtection extension. Please use wfLoadExtension ' .
  		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
  	);
  	return true;
